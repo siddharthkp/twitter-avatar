d0e102a7c6762f1fa8b75c0814c73acb93a2337d
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-08-21 16:04:37 +0530

    easy peasy

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# twitter-avatar
-Get twitter avatar from a username
+<p align="center">
+  <img src="https://cdn.dribbble.com/users/31506/screenshots/2621777/automatingtweets.png" height="200px"/>
+  <br><br>
+  <b>Get twitter avatar for a username</b>
+  <br><br>
+  Artwork by [Greg Grigoriou](https://dribbble.com/Greg_Grigoriou)
+</p>
+
+&nbsp;
+
+#### usage
+
+Hit [https://twitter-avatar.now.sh/{username}](https://twitter-avatar.now.sh/siddharthkp)
+
+&nbsp;
+
+#### like it?
+
+:star: this repo
+
+&nbsp;
+
+#### license
+
+MIT © [siddharthkp](https://github.com/siddharthkp)
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cheerio = require('cheerio')
+const request = require('request')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const cache = {}
+
+const get = username => {
+  const url = 'https://mobile.twitter.com/' + username
+  return new Promise((resolve, reject) => {
+    if (cache[username]) resolve(cache[username])
+    else
+      request(url, (error, res, body) => {
+        const $ = cheerio.load(body)
+        const url = ($('.avatar img').attr('src') || '').replace(
+          '_normal',
+          '_200x200'
+        )
+        cache[username] = url
+        resolve(url)
+      })
+  })
+}
+
+app.get('/:user', async (req, res, next) => {
+  const result = await get(req.params.user)
+  if (!result) return next(404)
+  request(result).pipe(res)
+})
+
+var listener = app.listen(3000, () => {
+  console.log('Your app is listening on port 3000')
+})
