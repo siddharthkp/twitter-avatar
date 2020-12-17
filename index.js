6e96e01d67135888256f38138b37863a2b36f901
PolarizedIons <polarizedions@gmail.com>
2020-12-17 19:37:56 +0200

    Update to new twitter - add headless browser to run js Twitter switched to a fully js-based website, breaking the current implementation. ZombieJS will evaluate the JS on new-twitter's page and find the profile picture that way.

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require("express");
 const cheerio = require("cheerio");
 const request = require("request");
 const cors = require("cors");
+const Zombie = require("zombie");
+var nock = require("nock")
+
+// Twitter takes surprisingly long to load
+Zombie.waitDuration = '15s'
+Zombie.userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0";
+Zombie.silent = true;
+
+// Don't load the following urls in Zombie
+nock('https://www.google-analytics.com')
+  .get(/.*/)
+  .times(Math.Infinity)
+  .reply(200, '{}')
+
+nock('https://app.link')
+  .get(/.*/)
+  .times(Math.Infinity)
+  .reply(200, '{}')
+
+nock('https://api2.branch.io')
+  .get(/.*/)
+  .times(Math.Infinity)
+  .reply(200, '{}')
 
 const app = express();
 app.use(cors());
@@ -10,19 +33,26 @@ const port = 3000;
 const cache = {};
 
 const get = (username, size) => {
-  const url = "https://mobile.twitter.com/" + username;
   return new Promise((resolve) => {
-    if (cache[username]) resolve(cache[username]);
-    else
-      request(url, (_, __, body) => {
-        const $ = cheerio.load(body);
-        const url = ($(".avatar img").attr("src") || "").replace(
-          "_normal",
-          size
-        );
-        cache[username] = url;
-        resolve(url);
-      });
+    if (cache[username + size]) resolve(cache[username + size]);
+    else {
+      const browser = new Zombie();
+      browser
+        .visit(`https://twitter.com/${username}/photo`)
+        .then(() => {
+          const $ = cheerio.load(browser.html());
+          const url = ($("img[src^='https://pbs.twimg.com/profile_images/'][src$='_400x400.jpg']").attr("src") || "").replace(
+            "_400x400",
+            size
+          );
+          cache[username + size] = url;
+          resolve(url);
+        })
+        .catch((err) => {
+          console.error("Failed to fetch profile", err);
+          resolve("");
+        });
+    }
   });
 };
 
